222da74d8c48854f7f46e5813e83fff578459df7
benliusf <benliusf@yahoo.com>
2026-03-24 18:18:58 -0700

    cleanup

--- pkg/send/builder_test.go
+++ pkg/send/builder_test.go
@@ -7,27 +7,12 @@ import (
 )
 
 func TestBuilder(t *testing.T) {
-	tests := []struct {
-		msgId             int32
-		msg               []byte
-		expectedMsgLength []byte
-		expectedMsgId     []byte
-		expectedMsgBytes  []byte
-	}{
-		{
-			msgId:             222,
-			msg:               []byte("hello mars"),
-			expectedMsgLength: []byte{0, 0, 0, 14},
-			expectedMsgId:     []byte{0, 0, 0, 222},
-			expectedMsgBytes:  []byte{104, 101, 108, 108, 111, 32, 109, 97, 114, 115},
-		},
-	}
-	for _, tt := range tests {
-		bd := newBuilder().withMsgId(tt.msgId).withMsgBytes(tt.msg)
-		actual, err := bd.build()
-		assert.Nil(t, err)
-		assert.Equal(t, []byte{0, 0, 0, 14}, actual[0:4])                                   // assert msg length
-		assert.Equal(t, []byte{0, 0, 0, 222}, actual[4:8])                                  // assert msgId
-		assert.Equal(t, []byte{104, 101, 108, 108, 111, 32, 109, 97, 114, 115}, actual[8:]) // assert msg bytes
-	}
+	msgId := int32(222)
+	msg := []byte("hello mars")
+	bd := newBuilder().withMsgId(msgId).withMsgBytes(msg)
+	actual, err := bd.build()
+	assert.Nil(t, err)
+	assert.Equal(t, []byte{0, 0, 0, 14}, actual[0:4])                                   // assert msg length
+	assert.Equal(t, []byte{0, 0, 0, 222}, actual[4:8])                                  // assert msgId
+	assert.Equal(t, []byte{104, 101, 108, 108, 111, 32, 109, 97, 114, 115}, actual[8:]) // assert msg bytes
 }
